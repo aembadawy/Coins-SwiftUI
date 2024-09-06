1ab2dc258b66be1d44bfada675c28389f20dbf3b
Aya E Mahmoud <ayae.mahmoud@gmail.com>
2024-09-06 08:13:10 +0300

    Initial Commit

---
+++ Coins-iOS/Coins_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coins_iOSApp.swift
+//  Coins-iOS
+//
+//  Created by Aya Mahmoud on 06/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct Coins_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
