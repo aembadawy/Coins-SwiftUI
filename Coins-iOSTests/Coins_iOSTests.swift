170197a52ab61f080b20c52787a6f428f441f7ac
Aya E Mahmoud <ayae.mahmoud@gmail.com>
2024-11-01 10:55:25 +0200

    Add unit testing

---
+++ Coins-iOSTests/Coins_iOSTests.swift
@@ -0,0 +1,16 @@
+//
+//  Coins_iOSTests.swift
+//  Coins-iOSTests
+//
+//  Created by Aya Mahmoud on 01/11/2024.
+//
+
+import Testing
+
+struct Coins_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
